822cf2c424e68a39f9486beb8f948ed89a1d591f
alexsan-dev <as62971@gmail.com>
2020-02-12 17:37:06 -0600

    Fix style

--- index.ts
+++ index.ts
@@ -31,11 +31,11 @@ const toast = (p:IToast) => {
 
   // AGREGAR CLASE 
   div.classList.add("toast");
-  div.style.cssText = "position: fixed;bottom: 0;left: 0;width: 100%;transform: translateY(100%);transition: transform 0.3s ease-in-out;background: #444;padding: 23px 20px;display: flex;align-items: center;justify-content: space-between;z-index: 99;"
+  div.style.cssText = "border-box:box-sizing;position: fixed;bottom: 0;left: 0;width: 100%;transform: translateY(100%);transition: transform 0.3s ease-in-out;background: #444;padding: 23px 20px;display: flex;align-items: center;justify-content: space-between;z-index: 99;"
 
   // ESTILOS
   span.style.cssText = "font-size: 1.1em;font-weight: bold;color:#fff;"
-  btn.style.cssText = "appearance: none;border: none;outline: none;background: rgba(0, 0, 0, .4);color:#fff;padding: 15px 25px;text-transform: uppercase;font-weight: bold;border-radius: 5px;cursor: pointer;"
+  btn.style.cssText = "position:relative;appearance: none;border: none;outline: none;background: rgba(0, 0, 0, .4);color:#fff;padding: 15px 25px;text-transform: uppercase;font-weight: bold;border-radius: 5px;cursor: pointer;"
 
   if (data.actionText) div.style.padding = "10px 20px";
   span.textContent = data.text;
