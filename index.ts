f172ae98b76b7ebeba92bd600938d2f46c965579
alexsan-dev <as62971@gmail.com>
2020-02-12 17:40:23 -0600

    Fix style

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ const toast = (p:IToast) => {
 
   // AGREGAR CLASE 
   div.classList.add("toast");
-  div.style.cssText = "border-box:box-sizing;position: fixed;bottom: 0;left: 0;width: 100%;transform: translateY(100%);transition: transform 0.3s ease-in-out;background: #444;padding: 23px 20px;display: flex;align-items: center;justify-content: space-between;z-index: 99;"
+  div.style.cssText = "box-sizing:border-box;position: fixed;bottom: 0;left: 0;width: 100%;transform: translateY(100%);transition: transform 0.3s ease-in-out;background: #444;padding: 23px 20px;display: flex;align-items: center;justify-content: space-between;z-index: 99;"
 
   // ESTILOS
   span.style.cssText = "font-size: 1.1em;font-weight: bold;color:#fff;"
